bb8ea851edad2df468966e806748c7b7c43471bf
FrustumsFire <frustumsfire@gmail.com>
2013-01-05 18:17:15 +0100

    Addong construction files.

---
+++ bin/build/Win32-gcc/core/build.bat
@@ -0,0 +1,21 @@
+@echo off
+
+REM The file is part of WitchEngine.
+REM Copyright (C) 2012 The Team Entertainment
+
+REM This program is free software: you can redistribute it and/or modify
+REM it under the terms of the GNU General Public License as published by
+REM the Free Software Foundation, either version 3 of the License, or
+REM (at your option) any later version.
+
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+
+REM You should have received a copy of the GNU General Public License
+REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+cmake -G "MinGW Makefiles"
+
+mingw32-make
